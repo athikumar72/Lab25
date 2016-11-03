5af88716fb97370537e73c08b82680f37bbe886c
athikumar72 <athikumar72@gmail.com>
2016-11-03 16:47:08 -0400

    Final commit

--- Lab25/Controllers/HomeController.cs
+++ Lab25/Controllers/HomeController.cs
@@ -3,16 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Lab25.Models;
 
 namespace Lab25.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        [HttpGet]
+        public ActionResult dataInput()
+
         {
             return View();
         }
 
+       [HttpPost]
+       public ActionResult dataInput(dataValidation validation)
+
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.greetings = "Hi I am a hard worker";
+                return View("display", validation);
+            }
+            else
+            {
+                return View();
+
+            }
+
+           
+        }
+      
+
+        public ActionResult  display(dataValidation validation)
+
+        {
+            ViewBag.Message = "Your application description page.";
+            return View(validation);
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
---
+++ Lab25/Models/dataValidation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace Lab25.Models
+{
+    public class dataValidation
+    {   [Required]     
+        public string Name { get; set; }
+       [Required]
+        public int age{ get; set; }
+    }
+}
